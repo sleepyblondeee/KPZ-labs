f6a5c1104d28b1979b52383859a4f812f7f4edbb
Kateryna Nechyporuk <vt231_nko@student.ztu.edu.ua>
2025-06-11 00:24:06 +0300

    Add lab-04 task 3 (Observer)

---
+++ lab-04/SupportLibrary/Level1Support.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupportLibrary
+{
+    public class Level1Support : SupportHandler
+    {
+        protected override bool AskUser()
+        {
+            Console.WriteLine("У вас питання про баланс, тарифи або пiдключення послуг? (1 - так, 0 - нi)");
+            string? input = Console.ReadLine();
+            if (input == "1")
+            {
+                Console.WriteLine("Рiвень 1 пiдтримки: загальнi запитання.");
+                return true;
+            }
+            return false;
+        }
+    }
+}
